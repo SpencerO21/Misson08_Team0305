31c85ad7b6cc400186f84a5f83908d1b48515863
spencer021 <spencerjolson@gmail.com>
2024-02-27 16:58:40 -0700

    Added repo architecture

---
+++ Misson08_Olson/Models/ITasksRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Misson08_Olson.Models
+{
+    public interface ITasksRepository
+    {
+        List<Task> tasks { get; }
+
+        public void AddTask(Task task);
+        public void RemoveTask(Task task);
+        public void UpdateTask(Task task);
+    }
+}
